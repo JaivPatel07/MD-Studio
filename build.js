c993253683465190ae55f666cc7378874932bed1
Jaiv Patel <jaivpatel402@gmail.com>
2026-07-25 16:41:08 +0530

    deploy

---
+++ build.js
@@ -0,0 +1,33 @@
+const fs = require('fs-extra');
+const { execSync } = require('child_process');
+
+const distFolder = 'dist';
+
+// List of files and folders to copy to the dist folder
+const filesToCopy = [
+  'index.html',
+  'style.css',
+  'manifest.json',
+  'icons', // This will copy the entire icons folder
+  'README.md' // Good to have in the deployment
+];
+
+async function build() {
+  try {
+    console.log('Cleaning old dist folder...');
+    await fs.emptyDir(distFolder);
+
+    console.log('Running esbuild...');
+    execSync('esbuild main.js --bundle --minify --outfile=dist/bundle.js', { stdio: 'inherit' });
+
+    console.log('Copying static assets...');
+    await Promise.all(filesToCopy.map(file => fs.copy(file, `${distFolder}/${file}`)));
+
+    console.log('Build successful!');
+  } catch (err) {
+    console.error('Build failed:', err);
+    process.exit(1);
+  }
+}
+
+build();
